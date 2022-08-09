c452b9b4b25279372986c5e7113aaa4550410356
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-09 22:37:50 +0300

    Task 60 completed

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿int[, ,] myArray = new int[2,2,2];
+for (int i =0; i < myArray.GetLength(0); i++)
+{
+   for (int j =0; j < myArray.GetLength(1); j++)
+   {
+    for (int k = 0; k < myArray.GetLength(2); k++)
+    {
+     myArray[i,j,k] = new Random().Next(100);   
+    }
+   }
+}
+for (int i =0; i < myArray.GetLength(0); i++)
+{
+   for (int j =0; j < myArray.GetLength(1); j++)
+   {
+    for (int k = 0; k < myArray.GetLength(2); k++)
+    {
+        Console.Write($"{myArray[i,j,k]}({i},{j},{k})  ");   
+    }
+    Console.WriteLine();
+   }
+
+}
